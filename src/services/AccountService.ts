cd44a6fb7ff282777054968e83cca4e9f0c97275
petrius <maria.petrius@gmail.com>
2025-07-26 17:33:06 +0200

    add createAcount in AccountService

--- src/services/AccountService.ts
+++ src/services/AccountService.ts
@@ -25,5 +25,15 @@ export async function getAccounById(id: number): Promise<Account> {
         console.log(`Error fetching account with Id: ${id}` , error);
         throw error;
     }
+}
 
+export async function createAccount(account: Account): Promise<Account> {
+    try {
+        const response = await axios.post<Account>('http://localhost:8081/api/v1/accounts', account);
+        return response.data;
+    }catch(error){
+        console.log(`Error creating the account ${account}` , error);
+        throw error;
+    }
+    
 }
