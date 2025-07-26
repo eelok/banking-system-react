590a86808fd0baaf2c4bfbd81cf55cd92ccea33b
petrius <maria.petrius@gmail.com>
2025-07-26 21:10:07 +0200

    add from java and from ts getListAccoutns

--- src/components/AccountList.tsx
+++ src/components/AccountList.tsx
@@ -1,56 +1,136 @@
 import { useEffect, useState } from 'react';
 import { Account } from '../types/Account';
-import { getListAccount } from '../services/AccountService';
+import { getListAccountFromTS } from '../services/AccountService';
+import { getListAccountFromJava } from '../services/AccountService';
 
 
 export default function AccountList() {
 
-  const [accounts, setAccounts] = useState<Account[]>([]);
-  const [error, setError] = useState<string | null>(null);
-  const [loading, setLoading] = useState<boolean>(false);
+  const [data, setData] = useState({
+    fromJava: { 
+      accounts: Array<Account>(), 
+      loading: true, 
+      error: null as string | null 
+    },
+    fromTS: {
+      accounts: Array<Account>(),
+      loading: true, 
+      error: null as string | null 
+    }
+  });
 
 
   useEffect( () => {
 
-    const fetchAccounts = async() => {
-      setError(null);
-      setLoading(true);
+    const getAccountsFromJava = async() => {
+      setData((prev) => ({
+        ...prev,
+        fromJava: {...prev.fromJava, loading: true, error: null}
+      }));
 
-      try {
-        const accounts = await getListAccount();
-        setAccounts(accounts);
+      try{
+        const accountsFromJava = await getListAccountFromJava();
+        setData((prev) => ({
+          ...prev,
+          fromJava: {accounts: accountsFromJava, loading: false, error: null}
+        }));
       } catch(error){
-        setError(String(error));
-      } finally {
-        setLoading(false);
+          setData((prev) => ({
+            ...prev,
+            fromJava: {accounts: [], loading: false, error: String(error)}
+          }));
+      } 
+  
+    };
+
+    const getAccountsFromTS = async() => {
+      setData((prev) => ({
+        ...prev,
+        fromTS: {...prev.fromTS, loading: true, error: null}
+      }));
+
+      try{
+        const accountFromTS = await getListAccountFromTS();
+        setData((prev) => ({
+          ...prev,
+          fromTS: {accounts: accountFromTS, loading: false, error: null}
+        }))
+      }catch(error){
+        setData((prev) => ({
+          ...prev,
+          fromTS: {accounts: [], loading: false, error: String(error)}
+        }));
       }
     }
 
-    fetchAccounts();
+
+    getAccountsFromJava();
+    getAccountsFromTS();
+  //   const fetchAccounts = async() => {
+  //     setError(null);
+  //     setLoading(true);
+
+  //     try {
+  //       const accounts = await getListAccountFromTS();
+  //       setAccounts(accounts);
+  //     } catch(error){
+  //       setError(String(error));
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   }
+
+  //   fetchAccounts();
     
   }, [])
 
   return (
     <div className="App">
+      <p>Java backend</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Name:</th>
+            <th>IBAN:</th>
+            <th>Details:</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.fromJava.accounts.map(account => (
+              <tr key={account.id}>
+                <td>{account.fullName}</td>
+                <td>{account.iban}</td>
+                <td>
+                  <a href={`/accounts/${account.id}`}>
+                    <strong>Click to see Accounts Details</strong>
+                  </a>
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>  
+      </table>
+
+      <p>TypeScript backend</p>
       <table>
         <thead>
           <tr>
             <th>Name</th>
             <th>IBAN</th>
-            <th>Currency</th>
-            <th>Balance</th>
-            <th>Withdraw Limit Per Day</th>
           </tr>
         </thead>
         <tbody>
           {
-            accounts.map(account => (
+            data.fromTS.accounts.map(account => (
               <tr key={account.id}>
                 <td>{account.fullName}</td>
                 <td>{account.iban}</td>
-                <td>{account.balance}</td>
-                <td>{account.currency}</td>
-                <td>{account.withdrawPerDayLimit}</td>
+                <td>
+                  <a href={`/accounts/${account.id}`}>
+                    <strong>Click to see Accounts Details</strong>
+                  </a>
+                </td>
               </tr>
             ))
           }
--- src/services/AccountService.ts
+++ src/services/AccountService.ts
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import { Account } from '../types/Account';
 
+const JAVA_API_URL = process.env.REACT_APP_JAVA_API_URL;
+const TS_API_URL = process.env.REACT_APP_TS_API_URL;
 
+export async function getListAccountFromTS(): Promise<Account[]>{
 
-export async function getListAccount(): Promise<Account[]>{
-
-    try {
-        const response =  await axios.get<Account[]>('http://localhost:8081/api/v1/accounts');
+    try {                                            
+        const response =  await axios.get<Account[]>(`${TS_API_URL}api/v1/accounts`);
         console.log(response.data);
         return response.data;
     } catch(error){
@@ -16,10 +17,32 @@ export async function getListAccount(): Promise<Account[]>{
  
 }
 
+export async function getListAccountFromJava(): Promise<Account[]> {
+    try {                                            
+        const response =  await axios.get<Account[]>(`${JAVA_API_URL}api/v1/accounts`);
+        console.log(response.data);
+        return response.data;
+    } catch(error){
+        console.error("Error fetching accounts: ", error);
+        throw error;
+    }
+}
+    
+
 
 export async function getAccounById(id: number): Promise<Account> {
     try{
-        const response = await axios.get<Account>(`http://localhost:8081/api/v1/accounts/${id}`);
+        const response = await axios.get<Account>(`${TS_API_URL}api/v1/accounts/${id}`);
+        return response.data;
+    } catch(error){
+        console.log(`Error fetching account with Id: ${id}` , error);
+        throw error;
+    }
+}
+
+export async function getAccounByIdFromJava(id: number): Promise<Account> {
+    try{
+        const response = await axios.get<Account>(`${JAVA_API_URL}api/v1/accounts/${id}`);
         return response.data;
     } catch(error){
         console.log(`Error fetching account with Id: ${id}` , error);
